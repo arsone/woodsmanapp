69eb284bbab33e383d6cc81252018a52473bc840
Adam Salberg <adsalberg@gmail.com>
2016-01-24 15:46:53 -0500

    Initial Commit

---
+++ WoodsmanPreshowApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WoodsmanPreshowApp
+//
+//  Created by Adam Salberg on 1/24/16.
+//  Copyright © 2016 Adam Salberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WoodsmanPreshowApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WoodsmanPreshowApp
+//
+//  Created by Adam Salberg on 1/24/16.
+//  Copyright © 2016 Adam Salberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
